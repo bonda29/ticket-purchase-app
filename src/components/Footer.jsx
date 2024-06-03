d6ec8c36814b4f48ecf34b962f113441fb96fd3b
bonda29 <bondableglobe10@gmail.com>
2024-06-03 23:20:57 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
 
     return (
         <footer>
-            <h3>2024 Национална природо-математическа гимназия</h3>
+            <h3>2024 Националната природо-математическа гимназия „Академик Любомир Чакалов“</h3>
             <h3>bonda.tech &#169; All rights reserved</h3>
         </footer>
     );
